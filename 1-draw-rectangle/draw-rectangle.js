2196045ce06df19851aee533d83a2e236c957740
Rino Jose <rjose@ejorp.com>
2012-11-11 09:01:04 -0800

    Pulled rectangles out into class Added function to convert rectangle to semantic value

--- 1-draw-rectangle/draw-rectangle.js
+++ 1-draw-rectangle/draw-rectangle.js
@@ -18,6 +18,9 @@
         Planman = root.Planman = {};
     }
     
+    // This is where we'll store the rectangles
+    Planman.rectangles = [];
+    
     // # Support functions
     
     // Creates an svg node. The `svgType` should be things like `svg`, `rect`, etc.
@@ -37,6 +40,16 @@
         return result;
     }
     
+    // This adds days to a date and returns the new date
+    function addDaysToDate(date, numDays) {
+        var msFactor = 24*60*60*1000;
+        var new_ms = date.getTime() + numDays*msFactor;
+        var result = new Date();
+        result.setTime(new_ms);
+        return result;
+    }
+    
+    
     // # Main functions
 
     // This function should be called once the page is finished loading. It sets up the svg
@@ -49,12 +62,64 @@
         var rect1 = newRectangle(0, 0, 50, 30);
         rect1.style.fill = "blue";
 
-        var rect2 = newRectangle(40, 40, 50, 30)
-        rect2.style.fill = "green";
+        var rect2 = newRectangle(40, 40, 75, 30)
+        rect2.style.fill = "purple";
+        
+        Planman.rectangles = [rect1, rect2];
 
-        [rect1, rect2].forEach(function(e) {
+        Planman.rectangles.forEach(function(e) {
             svgViewer.appendChild(e);
         })
+    }
+    
+
+    
+    // This is a proof-of-concept function for exporting the state of the work
+    // semantic JSON. The right way to do this would be to use a Backbone.Model
+    // and go the other direction :-).
+    Planman.rectAsJSON = function(rect) {
+        var result = {};
+        
+        // NOTE: Everything here is basically a hack
+        
+        // Assignee: corresponds to y value
+        if (rect.y.baseVal.value === 0) {
+            result.assignee = "Rino Jose";
+        }
+        else {
+            result.assignee = "Borvo Borvison";
+        }
+        
+        // Duration: corresponds to width
+        var numDays = rect.width.baseVal.value / 25;
+        result.duration = numDays;
+        
+        // Category: corresponds to fill style
+        var category = "Generic Work";
+        var blue = "#0000ff";
+        var purple = "#800080";
+        switch (rect.style.fill) {
+            case blue:            
+            category = "New Product Development";
+            break;
+            
+            case purple:
+            category = "Sustaining";
+            break;
+            
+            default:
+            break;
+        }
+        result.category = category;
+        
+        // Start date: corresponds to x
+        var referenceDate = new Date(2012, 10, 11);
+        var startDate = addDaysToDate(referenceDate, rect.x.baseVal.value/25)
+        var endDate = addDaysToDate(startDate, numDays);
+        result.startDate = startDate;
+        result.endDate = endDate;
+        
+        return result;
     }
 
 }).call(this);
